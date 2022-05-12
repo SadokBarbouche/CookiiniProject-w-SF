c10c5bea0428570a563bed03ff28e1ae988e0f9d
Baha <bahabouali.71@gmail.com>
2022-05-13 00:37:16 +0100

    updated

--- src/Controller/FoodComponentsController.php
+++ src/Controller/FoodComponentsController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Meal;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class FoodComponentsController extends AbstractController
 {
-    #[Route('/food_components', name: 'app_food_components')]
-    public function index(): Response
+    #[Route('/food_components/{id}', name: 'app_food_components')]
+    public function index(Meal $meal = null ): Response
     {
-        return $this->render('food_components/index.html.twig');
+        return $this->render('food_components/meal.html.twig', ['meal' => $meal]);
     }
 
 
